c35b660fb7ae1eb90bc8016151f579b9e4334c2e
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-03-16 14:32:50 +0100

    index.js marche coorrectement

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 ## Colorize black and white images using Deepai
 
 link to deepai colorizer :  
-https://deepai.org/machine-learning-model/colorizer
+https://deepai.org/machine-learning-model/colorizer  
+
+It takes around 5 SECONDS per image  
+
+## How to use:  
+
+- create a folder called input/  
+- put all your images (jpg or jpeg) in the folder  
+- run with node index.js and wait for the result  
--- index.js
+++ index.js
@@ -5,19 +5,43 @@ const request = require('request');
 deepai.setApiKey('00d2d1d1-f988-4edd-be31-08898ca3cbe2');
 
 (async function() {
-	const dir = "./colorized/";
-	if (!fs.existsSync(dir)) {
-		fs.mkdirSync(dir);
+	//initialise le dossier contenant les images sinon exit
+	const inputDir = "./input/";
+	if (!fs.existsSync(inputDir)) {
+		console.error("Can't find input directory");
+		process.exit(1);
 	}
-	let name = "0000";
-    var resp = await deepai.callStandardApi("colorizer", {
-            image: fs.createReadStream("./exemple/" + name +".jpeg"),
-    });
-	let url = resp.output_url;
-	let file = fs.createWriteStream(dir + name + ".jpg");
-    const sendReq = request.get(url);
-    sendReq.on('response', () => {
-        sendReq.pipe(file);
-    });
-    console.log("Done.");
+	//initialise le dossier de retour des images ou le créer s'il n'existe pas
+	const outputDir = "./colorized/";
+	if (!fs.existsSync(outputDir)) {
+		fs.mkdirSync(outputDir);
+	}
+	try {
+        // Get the files as an array
+        const files = await fs.promises.readdir(inputDir);
+
+        // Loop them all with the new foreach
+        for( const file of files ) {
+			//ask deepai to colorize image
+			var resp = await deepai.callStandardApi("colorizer", {
+					image: fs.createReadStream(inputDir + file),
+			});
+			//get url of deepai return
+			let url = resp.output_url;
+			//create a file with the image from the url
+			let newFile = fs.createWriteStream(outputDir + file);
+			const sendReq = request.get(url);
+			sendReq.on('response', () => {
+				sendReq.pipe(newFile);
+			});
+			//verbose mode
+			console.log(file);
+        }
+		//fin du programme
+		console.log("Done.");
+    }
+    catch( e ) {
+        // Catch anything bad that happens
+        console.error( "We've thrown! Whoops!", e );
+    }
 })()
